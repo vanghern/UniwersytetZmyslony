828d7f72136ab902257fe52c1975a83f01589700
Mateusz Ratajczak <vanghern2@gmail.com>
2016-04-11 14:59:19 +0200

    complex data

--- ContosoUniversity/Models/Instructor.cs
+++ ContosoUniversity/Models/Instructor.cs
@@ -5,32 +5,33 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
-    public class Student
+    public class Instructor
     {
         public int ID { get; set; }
+
         [Required]
-        [StringLength(50)]
         [Display(Name = "Nazwisko")]
+        [StringLength(50)]
         public string LastName { get; set; }
+
         [Required]
-        [StringLength(50, ErrorMessage = "Imię nie może być dłuższe niż 50 znaków")]
         [Column("FirstName")]
         [Display(Name = "Imię")]
+        [StringLength(50)]
         public string FirstMidName { get; set; }
+
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
-        [Display(Name = "Data Rekrutacji")]
-        public DateTime EnrollmentDate { get; set; }
+        [Display(Name = "Data Zatrudnienia")]
+        public DateTime HireDate { get; set; }
 
         [Display(Name = "Imię i Nazwisko")]
         public string FullName
         {
-            get
-            {
-                return LastName + ", " + FirstMidName;
-            }
+            get { return LastName + ", " + FirstMidName; }
         }
 
-        public virtual ICollection<Enrollment> Enrollments { get; set; }
+        public virtual ICollection<Course> Courses { get; set; }
+        public virtual OfficeAssignment OfficeAssignment { get; set; }
     }
 }
